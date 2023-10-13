e6d8b729cec7c52284dc292da81e6c1389eac397
Andrewab <andrewalcideblanchette@gmail.com>
2023-10-12 20:46:23 -0700

    newst vs

--- samplewebsite/src/app/page.tsx
+++ samplewebsite/src/app/page.tsx
@@ -2,13 +2,29 @@ import Header from './components/Header';
 import FrontPage from './components/FrontPage';
 import SecondPage from './components/SecondPage'
 import ThirdPage from './components/ThirdPage';
+import ImageCycler from './components/ImageCycler';
+//import {useClient} from 'next/react';
+
 export default function Home() {
+
+  const imagePaths = [
+    './images/Fireplace1.jpg',
+    './images/Fireplace2.jpg',
+  ];
   return (
     <div>
-      <Header/>
+      <Header/> 
       <FrontPage/>
       <SecondPage/>
-      <ThirdPage/>
+      <ThirdPage/> 
+          <img src={imagePaths[0]} layout={'fill'} objectFit={'contain'} alt="Image" />
     </div>
   )
 }
+
+/*
+<Header/>
+<FrontPage/>
+<SecondPage/>
+<ThirdPage/>
+*/
