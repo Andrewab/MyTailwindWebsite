603e517c29926288e5cf54edc903ecde84622d73
Andrewab <andrewalcideblanchette@gmail.com>
2024-04-04 15:15:39 -0700

    pushing new dropdown menuy!

---
+++ my-app/src/app/components/dropDownMenu.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React from 'react';
+
+const DropDownMenu = ({ onMouseEnter, onMouseLeave, showDropDownMenu }) => {
+  return (
+    <div className={`inline-block border-2 border-gray-800 relative left-2 dropdown-menu ${showDropDownMenu ? 'slide-down' : 'slide-up'}`} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <Link href="extraFiles/MyResume.pdf">
+        My Resume
+      </Link>
+      <style jsx>{`
+        @keyframes slide-down {
+          0% {
+            transform: scaleY(0);
+          }
+          100% {
+            transform: scaleY(1);
+          }
+        }
+
+        @keyframes slide-up {
+          0% {
+            transform: scaleY(1);
+          }
+          100% {
+            transform: scaleY(0);
+          }
+        }
+
+        .dropdown-menu {
+          animation-duration: 0.3s;
+          animation-timing-function: ease-out;
+          transform-origin: top;
+        }
+
+        .slide-down {
+          animation-name: slide-down;
+        }
+
+        .slide-up {
+          animation-name: slide-up;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default DropDownMenu;
--- my-app/src/app/components/navBar.tsx
+++ my-app/src/app/components/navBar.tsx
@@ -1,13 +1,17 @@
 'use client';
 import { useEffect, useState } from 'react';
-
+import Image from 'next/image';
+import DropDownMenu from './dropDownMenu';
 const Navbar = () => {
-  const [showHomeButton, setShowHomeButton] = useState(true);
+  //const [showHomeButton, setShowHomeButton] = useState(true);
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [isOverHomeButton, setIsOverHomeButton] = useState(false);
+  const [isOverDropDown, setIsOverDropDown] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       // Always set showHomeButton to true
-      setShowHomeButton(true);
+      //setShowHomeButton(true);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -20,19 +24,59 @@ const Navbar = () => {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
-
+  const handleLeave_ = (item:string)=> {
+    if (item == "dropDownMenu") {
+      setIsOverDropDown(false);
+      console.log("Not over DropDown");
+    }
+    else if (item == "homeButton") {
+      setIsOverHomeButton(false);
+      console.log("Not over Home button");
+    }
+    if(!isOverDropDown && !isOverHomeButton) {
+      console.log("removing dropdown");
+      setShowDropDown(false);
+    }
+    //console.log("checking dropdown: ",isOverDropDown, isOverHomeButton);
+  }
+  const handleLeave = () => {
+    if(!isOverDropDown) {
+      setShowDropDown(false);
+    }
+    console.log("leaving",isOverDropDown,isOverHomeButton);
+  };
   return (
     <div className="fixed top-0 left-0 w-full h-16 bg-blue-800 bg-opacity-95 flex items-center justify-between">
-      {showHomeButton && (
-        <div
+      <div className='fixed top-0'>
+        <Image
+          src="/images/menuBars.png"
+          width={65}
+          height={65}
+          alt="Home"
           className="pr-2 cursor-pointer italic text-white hover:underline text-2xl font-roboto font-semibold p-2"
           onClick={scrollToTop}
-        >
-          Home
-        </div>
-      )}
+          onMouseEnter={() => { setShowDropDown(true); setIsOverHomeButton(true);}}
+          onMouseLeave={() => {setIsOverHomeButton(false);handleLeave();}}
+        />
+        {showDropDown && (
+          <DropDownMenu
+            onMouseEnter={() => {setIsOverDropDown(true);if(!isOverDropDown){setShowDropDown(false)} }}
+            onMouseLeave={() => {setIsOverDropDown(false);setShowDropDown(false);}}
+            showDropDownMenu={showDropDown}
+          />
+        )}
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
+
+/*
+        <div
+          className="pr-2 cursor-pointer italic text-white hover:underline text-2xl font-roboto font-semibold p-2"
+          onClick={scrollToTop}
+        >Home</div>
+
+*/
