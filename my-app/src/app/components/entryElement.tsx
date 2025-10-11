0c608afbc8803355a078473ed2804f197e6be36b
Andrewab <andrewalcideblanchette@gmail.com>
2025-10-11 13:33:28 -0700

    more updates

--- my-app/src/app/components/entryElement.tsx
+++ my-app/src/app/components/entryElement.tsx
@@ -2,40 +2,42 @@
 
 const entryElement = () => {
     return (
-    <div className="flex p-10">
-        <div className="pb-10">
-            <div className="border-solid mx-auto w-2/6  bg-blue-800 bg-opacity-10">
-                    <div className="pl-10 pr-10 pt-5 text-center font-mono font-semibold text-2xl text-blue-800 ">
-                        <div >About me: </div>
-                    </div>
-                    <div className="p-5 text-l text-black font-mono">
-                        <p>
-                            CoFounder and head Unity Developer at Gandit Games Partnership.</p>
-                        <p className="pt-2"></p>
-                        <p>
-                            Focus in game development, UI/UX design, and interactive storytelling.</p>
-                        <p className="pt-2"></p>
-                        <p>
-                            If you are interested in collaborating or learning more about my work, feel free to reach out!</p>
-                    </div>
-            </div>
-        </div>
-        <div className="border-solid border-8 rounded-2xl border-black place-items-stretch mx-auto w-2/5  shadow-2xl flex  bg-blue-800 bg-opacity-70">
-            <div className="border-solid border-2 border-black flex-1">
-                <div className="pt-5 pb-5 text-center font-mono font-semibold text-4xl text-gray-200 bg-blue-600">
-                    <p>Skills</p>
+    <div className="flex justify-center">
+        <div className="grid grid-cols-2 gap-5 justify-center w-2/6">
+            <div className="">
+                <div className="shadow-xl mx-auto bg-blue-800 bg-opacity-10">
+                        <div className="pl-10 pr-10 pt-5 text-center font-mono font-semibold text-2xl text-blue-800 ">
+                            <div >About me: </div>
+                        </div>
+                        <div className="p-5 text-l text-black font-mono">
+                            <p>
+                                CoFounder and head Unity Developer at Gandit Games Partnership.</p>
+                            <p className="pt-2"></p>
+                            <p>
+                                Focus in game development, UI/UX design, and interactive storytelling.</p>
+                            <p className="pt-2"></p>
+                            <p>
+                                If you are interested in collaborating or learning more about my work, feel free to reach out!</p>
+                        </div>
                 </div>
-                <div className="p-10 flex-col text-gray-200 text-center font-mono text-xl align-top">
-                        <p className="font-bold">Languages & Scripting</p>
-                        <div>JavaScript - Python3 - Java - C - C# - SQLLite - Git - Bash</div>
-                        <p className="font-bold">Applications</p>
-                        <div>Excel - Adobe Premier Pro - Unity - Github - VSCode - Jira</div>
-                        <p className="font-bold">Operating Systems</p>
-                        <div>Windows 10 - MacOS - Ubuntu Linux</div>
-                        <p className="font-bold">Problem Solving</p>
-                        <div>List Comprehension - Algorithms - O(n) - Formal Logic(TFL)</div>
-                        <p className="font-bold">Workflow Strategies</p>
-                        <div>Scrum - AGILE Project Management</div>
+            </div>
+            <div className="shadow-xl bg-blue-800 bg-opacity-10">
+                <div className="">
+                    <div className="pl-10 pr-10 pt-5 text-center font-mono font-semibold text-2xl text-blue-800 ">
+                        <p>Skills: </p>
+                    </div>
+                    <div className="pl-5 pr-5 pt-5 text-l text-black font-mono text-center">
+                            <p className="font-bold">Languages & Scripting</p>
+                            <div>JavaScript - Python3 - Java - C - C# - SQLLite - Git - Bash</div>
+                            <p className="font-bold">Applications</p>
+                            <div>Excel - Adobe Premier Pro - Unity - Github - VSCode - Jira</div>
+                            <p className="font-bold">Operating Systems</p>
+                            <div>Windows 10 - MacOS - Ubuntu Linux</div>
+                            <p className="font-bold">Problem Solving</p>
+                            <div>List Comprehension - Algorithms - O(n) - Formal Logic(TFL)</div>
+                            <p className="font-bold">Workflow Strategies</p>
+                            <div>Scrum - AGILE Project Management</div>
+                    </div>
                 </div>
             </div>
         </div>
--- my-app/src/app/components/topElement.tsx
+++ my-app/src/app/components/topElement.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const TopElement = () => {
   return (
     <div className='mx-auto flex justify-center'>
-      <div className="font-roboto font-semibold text-3xl pb-4">Welcome To Andrew Blanchettes Website!</div>
+      <div className="font-mono font-semibold text-3xl pb-4">Andrew Blanchette's Website</div>
     </div>
   );
 }
