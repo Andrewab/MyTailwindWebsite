6136623190968f80b0239e0f2333797d7c259a18
Andrewab <andrewalcideblanchette@gmail.com>
2025-10-15 13:23:53 -0700

    cleaned up some code

--- my-app/src/app/components/menuButton.tsx
+++ my-app/src/app/components/menuButton.tsx
@@ -10,7 +10,6 @@ const MenuButton: React.FC<MenuButtonProps> = ({ text, link }) => {
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            // Toggle between "true" and "false" every 250 milliseconds
             setIsInvert(isInvert => !isInvert);
         }, 250);
 
@@ -41,21 +40,6 @@ const MenuButton: React.FC<MenuButtonProps> = ({ text, link }) => {
     </Link>
         );
     }
-
-    return (
-        // <div className={`outline-gray-800 outline ${isInvert ? "outline-dashed" : "outline-dotted"}`}>
-        //     <span className={`text-gray-800`}>
-        //         {text}
-        //     </span>
-        // </div>
-        <a href={link} target="_blank" rel="noopener noreferrer" className="group relative inline-block overflow-hidden rounded border border-gray-100 bg-gray-200  px-12 py-3 text-sm font-medium text-slate-800 hover:bg-gray-300 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white">
-	<span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
-	<span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
-	<span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
-	<span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
-	{text}
-</a>
-    );
 };
 
 export default MenuButton;
